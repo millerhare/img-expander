1ce570d11f7c5e11b7a04a19961426397bf242c4
orangemug <info@orangemug.co.uk>
2014-09-01 17:54:03 +0100

    Major updates.

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ var ie = require("./");
 
 var el1 = document.querySelector(".test1");
 var el2 = document.querySelector(".test2");
+var selectEl = document.querySelector("select");
+
+
+selectEl.addEventListener("change", function(e) {
+  if( ie.isExpanded(el1) ) {
+    var container = document.querySelector("."+selectEl.value);
+    ie.expand(el1, container);
+  }
+});
+
 
 el1.addEventListener("click", function(e) {
-  var selectEl = document.querySelector("select");
   var container = document.querySelector("."+selectEl.value);
   ie.expand(e.target, container);
-})
+});
 
 el1.addEventListener("expander-proxy:click", function(e) {
   ie.collapse(e.detail.target);
-})
+});
 
--- index.js
+++ index.js
@@ -1,37 +1,63 @@
+var debounce = require("lodash.debounce");
 var proxy = require("./proxy");
 
 var uid   = 0;
 var store = {};
 
-function expand(el, cEl) {
+function expand(el, cEl, opts) {
+  opts = opts || {};
+  opts.pos = opts.pos || "center";
+
+  if(opts.animation === undefined) {
+   opts.animation = true;
+  }
+
+  var id = el.getAttribute("data-id");
   var src = el.src;
-  var x   = el.offsetLeft;
-  var y   = el.offsetTop;
+  var rect = el.getBoundingClientRect();
+  var x   = rect.left + window.scrollX;
+  var y   = rect.top  + window.scrollY;
   var w   = el.offsetWidth;
   var h   = el.offsetHeight;
 
-  var cx = cEl.offsetLeft;
-  var cy = cEl.offsetTop;
+  var rect = cEl.getBoundingClientRect();
+  var cx   = rect.left + window.scrollX;
+  var cy   = rect.top  + window.scrollY;
   var cw = cEl.offsetWidth;
   var ch = cEl.offsetHeight;
 
   var interval = 500;
 
-  var nEl = document.createElement("div");
+
+
+  var nEl;
+  if(id === null) {
+    nEl = document.createElement("div");
+  } else {
+    nEl = store[id].child;
+  }
+
   nEl.style.position       = "absolute";
-  nEl.style.left           = x+"px";
-  nEl.style.top            = y+"px";
+  nEl.style.top            = "0px";
+  nEl.style.left           = "0px";
+  nEl.style.webkitTransformOrigin = "top left";
   nEl.style.width          = w+"px";
   nEl.style.height         = h+"px";
   nEl.style.background     = "url("+src+")";
   nEl.style.backgroundSize = "contain";
+  nEl.style.backgroundPosition = "top left";
   nEl.style.backgroundRepeat = "no-repeat";
-  nEl.style.backgroundPosition = "left center"; 
-  nEl.style.webkitTransition =
-      "  width  "+interval+"ms"
-    + ", height "+interval+"ms"
-    + ", top    "+interval+"ms"
-    + ", left   "+interval+"ms";
+
+  if(opts.animation) {
+    nEl.style.webkitTransition =
+      "-webkit-transform "+interval+"ms";
+  }
+
+  if(id === null) {
+    // Don't do it again!
+    nEl.style.webkitTransform = "translate("+x+"px, "+y+"px)";
+  }
+
 
   uid++;
 
@@ -50,13 +76,69 @@ function expand(el, cEl) {
 
   document.body.appendChild(nEl);
 
+  scalew = cw / w;
+  scaleh = ch / h;
+  if(scaleh < scalew) {
+    scale = scaleh;
+  } else {
+    scale = scalew;
+  }
+
+  var tx, ty;
+
+  var pos = opts.pos.split(/\s+/);
+  if(pos.length < 2) {
+    pos[1] = pos[0];
+  }
+
+  var posx = pos[0];
+  var posy = pos[1];
+
+  if(posx === "right") {
+    tx = parseInt(cx, 10) + parseInt(cw, 10) - parseInt(w*scale, 10);
+  } else if(posx === "center") {
+    tx =  parseInt(cx, 10) + parseInt(cw, 10)/2 - parseInt(w*scale, 10)/2;
+  } else {
+    // LEFT
+    tx = cx;
+  }
+
+  if(posy === "bottom") {
+    ty = parseInt(cy, 10) + parseInt(ch, 10) - parseInt(h*scale, 10);
+  } else if(posy === "center") {
+    ty =  parseInt(cy, 10) + parseInt(ch, 10)/2 - parseInt(h*scale, 10)/2;
+  } else {
+    // TOP
+    ty = cy;
+  }
+
   setTimeout(function() {
-    nEl.style.left   = cx+"px";
-    nEl.style.top    = cy+"px";
-    nEl.style.width  = cw+"px";
-    nEl.style.height = ch+"px";
+    nEl.style.webkitTransform = "translate("+tx+"px, "+ty+"px) scale("+scale+")";
   }, 0);
 
+  function resize() {
+    opts.animation = false;
+    expand(el, cEl, opts);
+    nEl.style.opacity = 1;
+  }
+
+  resize = debounce(resize, 100, {
+    trailing: true
+  });
+
+  window.addEventListener("resize", function(e) {
+    nEl.style.opacity = 0;
+    resize();
+  }, false);
+
+  if(opts.animation) {
+    nEl.addEventListener("webkitTransitionEnd", function fn() {
+      nEl.style.webkitTransition = "";
+      nEl.removeEventListener("webkitTransitionEnd", fn);
+    });
+  }
+
+  return nEl;
 }
 
 function toggle(el, cEl) {
@@ -76,31 +158,36 @@ function collapse(el) {
   var interval = 500;
 
   var src = el.src;
-  var x   = el.offsetLeft;
-  var y   = el.offsetTop;
+  var rect = el.getBoundingClientRect();
+  var x   = rect.left + window.scrollX;
+  var y   = rect.top  + window.scrollY;
   var w   = el.offsetWidth;
   var h   = el.offsetHeight;
 
-  nEl.style.left           = x+"px";
-  nEl.style.top            = y+"px";
-  nEl.style.width          = w+"px";
-  nEl.style.height         = h+"px";
+  nEl.style.webkitTransform = "translate("+x+"px, "+y+"px) scale(1)";
 
   nEl.addEventListener("webkitTransitionEnd", function fn() {
     el.style.opacity = 1;
     nEl.parentNode.removeChild(nEl);
+    el.removeAttribute("data-id");
     nEl.removeEventListener("webkitTransitionEnd", fn);
   });
 
   nEl.style.webkitTransition =
-      "  width  "+interval+"ms"
-    + ", height "+interval+"ms"
-    + ", top    "+interval+"ms"
-    + ", left   "+interval+"ms";
+    "-webkit-transform    "+interval+"ms"
+}
+
+function isExpanded(el) {
+  var id = el.getAttribute("data-id");
+  if(id && store[id]) {
+    return store[id];
+  }
+  return false;
 }
 
 module.exports = {
   expand:   expand,
+  isExpanded: isExpanded,
   collapse: collapse,
   toggle:   toggle
 };
