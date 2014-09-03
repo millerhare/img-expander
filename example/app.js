766c3a1ea6236a370e3b1e6f8da83a7a9339fff1
orangemug <info@orangemug.co.uk>
2014-09-03 09:33:29 +0100

    Added example + fixes.

--- example/app.js
+++ example/app.js
@@ -1,24 +1,19 @@
-var ie = require("../");
+var Delegate = require("dom-delegate");
+var ie       = require("../");
 
-var el1 = document.querySelector(".test1");
-var el2 = document.querySelector(".test2");
-var selectEl = document.querySelector("select");
+var container = document.querySelector(".target");
+var prev;
 
-
-selectEl.addEventListener("change", function(e) {
-  if( ie.isExpanded(el1) ) {
-    var container = document.querySelector("."+selectEl.value);
-    ie.expand(el1, container);
+var delegate = new Delegate(document.body);
+delegate.on('click', '.images img', function(e, target) {
+  if(prev) {
+    ie.collapse(prev);
   }
+  prev = target;
+  ie.expand(target, container);
 });
 
-
-el1.addEventListener("click", function(e) {
-  var container = document.querySelector("."+selectEl.value);
-  ie.expand(e.target, container);
-});
-
-el1.addEventListener("img-expander:click", function(e) {
-  ie.collapse(e.detail.target);
+delegate.on('img-expander:click', '.images img', function(e, target) {
+  ie.collapse(target);
 });
 
